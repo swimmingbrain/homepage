a1626f2384ea977786cfef8ceaef77d667ed601e
Braian <braian.plaku@gmail.com>
2024-06-16 15:37:09 +0200

    Initial

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyNewApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
